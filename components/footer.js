b97f8e5956bf4064e1b8eccdc781fb88e42d6e76
Murat Yüksel <muratyukse@gmail.com>
2022-01-09 22:13:51 +0100

    back to static html

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return <footer className="text-gray-600 body-font">
         <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
             <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">©
-                2021 Murat Yüksel
+                2022 Murat Yüksel
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                 <a href="https://github.com/muratyukselnet" target="_blank" className="text-gray-500" title="Github">
